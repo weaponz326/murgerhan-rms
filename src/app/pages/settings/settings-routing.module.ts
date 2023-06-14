ead0974cc71e324df439674ad03d7d33e866bedd
Weps <weaponz326@gmail.com>
2023-06-14 03:52:17 +0000

    user settings design

--- src/app/pages/settings/settings-routing.module.ts
+++ src/app/pages/settings/settings-routing.module.ts
@@ -3,9 +3,9 @@ import { RouterModule, Routes } from '@angular/router';
 
 import { SettingsPage } from './settings.page';
 import { BasicProfileComponent } from './basic-profile/basic-profile.component';
-import { AdditionalProfileComponent } from './additional-profile/additional-profile.component';
 import { AvailabilityComponent } from './availability/availability.component';
 import { TermsComponent } from './terms/terms.component';
+import { AdditionalInfoComponent } from './additional-info/additional-info.component';
 
 
 const routes: Routes = [
@@ -13,8 +13,9 @@ const routes: Routes = [
     path: "",
     component: SettingsPage,
     children: [
+      { path: "", component: BasicProfileComponent },
       { path: "basic", component: BasicProfileComponent },
-      { path: "additional", component: AdditionalProfileComponent },
+      { path: "additional", component: AdditionalInfoComponent },
       { path: "availability", component: AvailabilityComponent },
       { path: "terms", component: TermsComponent },
     ]
--- src/app/pages/settings/settings.module.ts
+++ src/app/pages/settings/settings.module.ts
@@ -4,18 +4,18 @@ import { CommonModule } from '@angular/common';
 import { SettingsRoutingModule } from './settings-routing.module';
 import { SettingsPage } from './settings.page';
 import { BasicProfileComponent } from './basic-profile/basic-profile.component';
-import { AdditionalProfileComponent } from './additional-profile/additional-profile.component';
 import { AvailabilityComponent } from './availability/availability.component';
 import { TermsComponent } from './terms/terms.component';
+import { AdditionalInfoComponent } from './additional-info/additional-info.component';
 
 
 @NgModule({
   declarations: [
     SettingsPage,
     BasicProfileComponent,
-    AdditionalProfileComponent,
     AvailabilityComponent,
-    TermsComponent
+    TermsComponent,
+    AdditionalInfoComponent
   ],
   imports: [
     CommonModule,
