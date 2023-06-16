61b65e9c7697571e902f2556c4dc39bedbff6740
Weps <weaponz326@gmail.com>
2023-06-16 16:32:06 +0000

    tested auth

--- src/app/components/main-navbar/main-navbar.component.ts
+++ src/app/components/main-navbar/main-navbar.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { AuthApiService } from 'src/app/services/auth-api/auth-api.service';
 
 @Component({
   selector: 'app-main-navbar',
@@ -6,5 +7,60 @@ import { Component } from '@angular/core';
   styleUrls: ['./main-navbar.component.scss']
 })
 export class MainNavbarComponent {
+
+  constructor(
+    private authApi: AuthApiService,
+  ) { }
+
+  isLoggedIn: boolean = false;
+  isAuthLoading: boolean = false;
+
+  name: string = "";
+  email: string = "";
+  photo: string = "../../../../assets/images/utilities/photo-avatar.jpg";
+
+  ngOnInit(): void {
+    this.getAuth();
+  }
+
+  getAuth(){
+    this.isAuthLoading = true;
+
+    this.authApi.getAuth()
+      .subscribe(
+        (res: any) => {
+          console.log(res);
+          this.isAuthLoading = false;
+
+          localStorage.setItem('uid', res.uid);
+
+          if (res.uid){
+            this.isLoggedIn = true;
+            this.email = res.email;
+          }
+        },
+        (err: any) => {
+          console.log(err);
+          this.isLoggedIn = false;
+          this.isAuthLoading = false;
+        }
+      )
+  }
+
+  logout(){
+    // e.stopPropagation();
+    console.log("u logging out? ...where u going?");
+
+    this.authApi.logout()
+      .then(
+        (res: any) => {
+          console.log(res);
+          window.location.href = "/";
+        },
+        (err: any) => {
+          console.log(err);
+        }
+      )
+  }
 
 }
--- src/app/pages/auth/login-form/login-form.component.ts
+++ src/app/pages/auth/login-form/login-form.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { FormControl, FormGroup, Validators } from '@angular/forms';
+import { Router } from '@angular/router';
 
 import { AuthApiService } from 'src/app/services/auth-api/auth-api.service';
 
@@ -12,6 +13,7 @@ import { AuthApiService } from 'src/app/services/auth-api/auth-api.service';
 export class LoginFormComponent {
 
   constructor(
+    private router: Router,
     private authApi: AuthApiService,
   ) { }
 
@@ -33,28 +35,25 @@ export class LoginFormComponent {
     let email = this.loginForm.controls.email.value as string
     let password = this.loginForm.controls.password.value as string
 
-    if (this.loginForm.valid){
-      this.isSending = true;
+    this.isSending = true;
 
-      this.authApi.login(email, password)
-        .then(
-          (res: any) => {
-            console.log(res);
-            this.isSending = false;
-            // user = res.user.uid;
-          },
-          (err: any) => {
-            console.log(err);
-            this.isSending = false;
-            this.errorMessage = err.message.replace("Firebase:", "").replace(/\(.*\)/, "").trim().replace(/\.$/, "");
-            this.errorCode = err.code;
-            console.log(this.errorCode, this.errorMessage)
-          }
-        )
-    }
-    else{
-      console.log("form is invalid");
-    }
+    this.authApi.login(email, password)
+      .then(
+        (res: any) => {
+          console.log(res);
+          this.isSending = false;
+          
+          localStorage.setItem('uid', res.user.uid);
+          this.router.navigateByUrl('/')
+        },
+        (err: any) => {
+          console.log(err);
+          this.isSending = false;
+          this.errorMessage = err.message.replace("Firebase:", "").replace(/\(.*\)/, "").trim().replace(/\.$/, "");
+          this.errorCode = err.code;
+          console.log(this.errorCode, this.errorMessage)
+        }
+      )
   }
 
 }
