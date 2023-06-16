be0babe4bf05f4fe548b8800d87ae23a96a0cf16
Weps <weaponz326@gmail.com>
2023-06-16 05:47:12 +0000

    added firebase

---
+++ src/environments/environment.ts
@@ -0,0 +1,11 @@
+export const environment = {
+    firebase: {
+        apiKey: "AIzaSyBplhMkQUMesRiULbqb4rcHCQyJH6ee6kE",
+        authDomain: "murgerhan.firebaseapp.com",
+        projectId: "murgerhan",
+        storageBucket: "murgerhan.appspot.com",
+        messagingSenderId: "250103945305",
+        appId: "1:250103945305:web:757454e303f297f2e3b948",
+        measurementId: "G-54L510RTRR"
+    }
+};
